1ecb05bf00460b7d05b5b6b68ecbb80854c1deeb
Zack Schrag <zack.schrag@factioninc.com>
2017-06-29 16:53:33 -0600

    Added options to scripts for easier development.

--- starter_files/base/schemas.py
+++ starter_files/base/schemas.py
@@ -8,6 +8,6 @@ from app import ma
 class BaseSchema(ma.ModelSchema):
     @pre_load
     def auto_generate_id(self, data):
-        if data.get("identifier") is None:
+        if isinstance(data, dict) and data.get("identifier") is None:
             data["identifier"] = str(uuid.uuid4())
         return data
